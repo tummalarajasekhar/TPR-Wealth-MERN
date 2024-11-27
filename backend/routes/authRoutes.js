480580ab4a6853d0f1a5fbe3790635a772473585
tummalarajasekhar <trsr.rajasekhar@gmail.com>
2024-11-27 18:32:00 +0530

    package.json

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
+
 const jwt = require("jsonwebtoken");
 const User = require("../models/User"); // Ensure you have a User model
 const nodemailer = require("nodemailer"); // For sending emails
